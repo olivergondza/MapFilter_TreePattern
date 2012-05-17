4e48291cde99db0d86d10e3b756a634d2048f2ff
Oliver Gondža <324706@mail.muni.cz>
2012-05-17 08:24:13 +0200

    direction test cleaned

--- tests/User/TreePattern/Direction.test.php
+++ tests/User/TreePattern/Direction.test.php
@@ -12,110 +12,118 @@ require_once 'tests/Functional.php';
 class MapFilter_Test_User_TreePattern_Direction extends
     MapFilter_TreePattern_Test_Functional
 {
-  
+
   /**@{*/
   public function provideDirection () {
-  
+
     return Array (
         // Single direction
         Array (
+            /** TreePattern_Direction_Valid_One__ */
             Array (
-                'top' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
             ),
+            /** __TreePattern_Direction_Valid_One */
             Array (
-                'top' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
             ),
         ),
         // Two compatible directions
         Array (
+            /** TreePattern_Direction_Valid_Two__ */
             Array (
-                'top'  => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'left' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'west' => Array ( 'unit' => 'yard',  'count' => 1 ),
             ),
+            /** __TreePattern_Direction_Valid_Two */
             Array (
-                'top'  => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'left' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'west' => Array ( 'unit' => 'yard',  'count' => 1 ),
             ),
         ),
         // Two incompatible directions; one will be trimmed
         Array (
+            /** TreePattern_Direction_Invalid_Two__ */
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'bottom' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'south' => Array ( 'unit' => 'yard',  'count' => 1 ),
             ),
+            /** __TreePattern_Direction_Invalid_Two */
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
             ),
         ),
         // Three directions; one will be trimmed
         Array (
+            /** TreePattern_Direction_Invalid_Three__ */
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'bottom' => Array ( 'unit' => 'yard',  'count' => 1 ),
-                'left'   => Array ( 'unit' => 'meter', 'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'south' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'west' => Array ( 'unit' => 'meter', 'count' => 1 ),
             ),
+            /** __TreePattern_Direction_Invalid_Three */
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'left'   => Array ( 'unit' => 'meter', 'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'west' => Array ( 'unit' => 'meter', 'count' => 1 ),
             ),
         ),
         Array (
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'right'  => Array ( 'unit' => 'yard',  'count' => 1 ),
-                'left'   => Array ( 'unit' => 'meter', 'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'east' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'west' => Array ( 'unit' => 'meter', 'count' => 1 ),
             ),
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'right'  => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'north'=> Array ( 'unit' => 'meter', 'count' => 2 ),
+                'east' => Array ( 'unit' => 'yard',  'count' => 1 ),
             ),
         ),
         // Four directions; two will be trimmed
         Array (
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'bottom' => Array ( 'unit' => 'yard',  'count' => 1 ),
-                'right'  => Array ( 'unit' => 'yard',  'count' => 1 ),
-                'left'   => Array ( 'unit' => 'meter', 'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'south' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'east' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'west' => Array ( 'unit' => 'meter', 'count' => 1 ),
             ),
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'right'  => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'east' => Array ( 'unit' => 'yard',  'count' => 1 ),
             ),
         ),
         Array (
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'left'   => Array ( 'unit' => 'meter', 'count' => 1 ),
-                'bottom' => Array ( 'unit' => 'yard',  'count' => 1 ),
-                'right'  => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'west' => Array ( 'unit' => 'meter', 'count' => 1 ),
+                'south' => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'east' => Array ( 'unit' => 'yard',  'count' => 1 ),
             ),
             Array (
-                'top'    => Array ( 'unit' => 'meter', 'count' => 2 ),
-                'right'  => Array ( 'unit' => 'yard',  'count' => 1 ),
+                'north' => Array ( 'unit' => 'meter', 'count' => 2 ),
+                'east' => Array ( 'unit' => 'yard',  'count' => 1 ),
             ),
         ),
     );
   }
   /**@}*/
-  
+
   /**
    * @dataProvider      provideDirection
    */
   public function testDirection ( $query, $result ) {
-  
+
     $pattern = MapFilter_TreePattern_Xml::fromFile (
         PHP_TREEPATTERN_TEST_DIR . MapFilter_Test_Sources::DIRECTION
     );
 
     $this->assertResultsEquals ( $pattern, $query, $result );
   }
-  
+
   /**
    * @dataProvider      provideDirection
    */
   public function testNewDirection ( $query, $result ) {
-  
+
     $pattern = MapFilter_TreePattern_Xml::fromFile (
         PHP_TREEPATTERN_TEST_DIR . MapFilter_Test_Sources::DIRECTION_NEW
     );
